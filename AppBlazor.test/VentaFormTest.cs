d7c2e880a100b2c278464bb61fa5794767414cc6
sandramamanif3 <sandramamanif3@gmail.com>
2025-08-17 21:09:24 -0400

    Modificación del test

--- AppBlazor.test/VentaFormTest.cs
+++ AppBlazor.test/VentaFormTest.cs
@@ -13,9 +13,44 @@ namespace AppBlazor.test
             return resultados;
         }
         [Fact]
-        public void ValidarCampos()
+        public void ValidacionFallaCuandoCamposVacios()
         {
+            var venta = new VentasCLS();
 
+            var errores = ValidarModelo(venta);
+
+            Assert.Contains(errores, e => e.ErrorMessage!.Contains("El número de empleado es requerido"));
+
+            Assert.Contains(errores, e => e.ErrorMessage!.Contains("El nombre es requerido"));
+
+            Assert.Contains(errores, e => e.ErrorMessage!.Contains("La edad debe ser un número mayor o igual a 18"));
+
+            Assert.Contains(errores, e => e.ErrorMessage!.Contains("El cargo es requerido"));
+
+            Assert.Contains(errores, e => e.ErrorMessage!.Contains("La fecha de contrato es requerida"));
+
+            Assert.Contains(errores, e => e.ErrorMessage!.Contains("La cuota es requerida"));
+
+            Assert.Contains(errores, e => e.ErrorMessage!.Contains("Las ventas son requeridas"));
+
+        }
+        [Fact]
+        public void ValidacionDatosCorrectos()
+        {
+            var venta = new VentasCLS
+            {
+                Num_Empl = 1,
+                Nombre = "Erick Chambi",
+                Edad = 18,
+                Cargo = "Vendedor",
+                FechaContrato = new DateTime(2025, 5, 10),
+                Cuota = 60,
+                Ventas = 10
+            };
+
+            var errores = ValidarModelo(venta);
+
+            Assert.Empty(errores);
         }
     }
 }
